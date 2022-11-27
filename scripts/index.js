9a29d8dc790763fffdc6b3a3231a9522117bfb4a
Dmitry <Ezzy-Way@yandex.ru>
2022-11-27 20:59:32 +0400

    add/template

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const taskList = document.querySelector('.list__tasks');
+// const taskTemplate = document.querySelector('#template').content.querySelector('.list__task');
+const taskForm = document.querySelector('.tasks__form');
+const taskInput = taskForm.querySelector('.tasks__input');
